adf8397caea1da9f0e6641f3afea8b6b1391a345
16kohnegrant <16kohnegrant@WHS-F203-08.ad.iusd.org>
2016-05-25 12:24:22 -0700

    Merge origin/master
    Conflicts:
    	Source/src/NotDoom/GUI.java
    	Source/src/NotDoom/Map/Map.java

--- Source/src/NotDoom/Renderer/DoomRenderer.java
+++ Source/src/NotDoom/Renderer/DoomRenderer.java
@@ -14,10 +14,13 @@
  * limitations under the License.
  */
 package NotDoom.Renderer;
+import NotDoom.IntVector;
 import NotDoom.Map.*;
 import NotDoom.Player;
 import NotDoom.Vector;
+import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 /**
  *
@@ -29,12 +32,16 @@ public class DoomRenderer {
     private int[] backBuffer;
     private final int WIDTH;
     private final int HEIGHT;
+    private ArrayList<Wall> walls;
 
     public DoomRenderer(BufferedImage buffer) {
         this.buffer = buffer;
         WIDTH = buffer.getWidth();
         HEIGHT = buffer.getHeight();
         backBuffer = new int[WIDTH * HEIGHT];
+        walls = new ArrayList<>();
+        
+        
     }
 
     public void DrawRegion(Region r) {
@@ -57,6 +64,31 @@ public class DoomRenderer {
         int x2 = (int) ((WIDTH / 2) + (float) (WIDTH / 2) * (v2.x() / v2.y()));
         int y1 = (int) ((HEIGHT / 2) + (float) (HEIGHT / 2) * (p.getHeight() - floor) / v1.y());
 
+    }
+    
+    public void DrawMap(Map m){
+        Region [] r = m.getRegion();
+              
+        for (int i = 0; i < r.length; i++){
+                System.out.println("IM COCO FOR COCOPUFFS");
+                
+                for (int j = 0; j < r[i].getWalls().length;j++){
+                    System.out.println(r[i].getWalls().length+" Walls");
+                    walls.add(r[i].getWalls()[j]);                
+                }
+        }
+        
+        for (int j = 0; j < walls.size(); j++){
+                //System.out.println("HEllo");
+                IntVector v1, v2;
+                v1 = walls.get(j).v1();
+                v2 = walls.get(j).v2();
+                
+                DrawLine(v1.x(),v1.y(),v2.x(),v2.y(), 0xFFFFFF);
+                
+                    
+            }
+        
     }
 
     public void DrawLine(int x1, int x2, int y1, int y2, int color){
